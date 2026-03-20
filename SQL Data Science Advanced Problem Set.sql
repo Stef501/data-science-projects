42f6b10298c194ccf58f795ae75e07ac43f05fc3
Stef501 <78814589+Stef501@users.noreply.github.com>
2026-03-19 22:03:36 -0400

    Add files via upload

--- SQL Data Science Advanced Problem Set.sql
+++ SQL Data Science Advanced Problem Set.sql
@@ -102,6 +102,7 @@ ON u.user_id = e.user_id
 GROUP BY u.signup_date
 ORDER BY u.signup_date;
 
+--Comments--
 -- Count(distinct case) is used to apply a condition inside an aggregation without filtering the entire dataset.
 -- WHERE is cannot be used beause it filters rows BEFORE aggregation and sometimes we want one metric using all rows and another metric using only some rows.
 -- Left Join is used because users with no events must still exist in cohort.
@@ -111,63 +112,93 @@ select
 (select count(distinct(p.user_id))::float
 from purchases p
 join users u on u.user_id = p.user_id
-where p.purchase_date between u.signup_date and u.signup_date + interval '7 days')
+where p.purchase_date > u.signup_date AND p.purchase_date <= u.signup_date + INTERVAL '7 days')
 /
 (select count(distinct(user_id)) from users) as conversion_rate;
 
 --Q3. Calculate the funnel conversion rates: view → add_to_cart → purchase
---Method 1
-Select *, (users)/lag(users) over (order by 
-CASE stages
-    WHEN 'view' THEN 1
-    WHEN 'add_to_cart' THEN 2
-    WHEN 'purchases' THEN 3
-END)::float as conversion_from_previous
-from
-(select event_type as stages, count(distinct(user_id)) as users
-from events
-group by event_type 
-ORDER BY CASE event_type
-    WHEN 'view' THEN 1
-    WHEN 'add_to_cart' THEN 2
-    WHEN 'purchases' THEN 3
-END);
 
---Method 2
+--Method 1 assuming tight funnel
+with user_funnel as
+	(select user_id,
+		max(case when event_type='view' then 1 end) as viewer,
+		max(case when event_type='add_to_cart' then 1 end) as adder,
+		max(case when event_type='purchases' then 1 end) as purchaser
+		from events
+		group by user_id),
+
+funnel_counts as(
+	select 'view' as stage, count(*) as users
+	from user_funnel
+	where viewer=1
+
+	union all
+
+	select 'add_to_cart' as stage, count(*)
+	from user_funnel
+	where viewer=1 and adder=1
+
+	union all
+	
+	select 'purchases' as stage, count(*)
+	from user_funnel
+	where viewer=1 and adder=1 and purchaser=1)
+
+select
+	stage,
+	users,
+	users/lag(users) over(
+	order by case stage 
+	when 'view' then 1
+	when 'add_to_cart' then 2
+	when 'purchases' then 3
+	end)::float as conversion_from_previous
+	from funnel_counts;
+		
+--Method 2 assuming loose funnel
 select event_type as stages, count(distinct(user_id)) as users,
 count(distinct(user_id))/lag(count(distinct(user_id))) over(
 ORDER BY CASE event_type
     WHEN 'view' THEN 1
     WHEN 'add_to_cart' THEN 2
     WHEN 'purchases' THEN 3
-END)::float as concersion_from_previous
+END)::float as conversion_from_previous
 from events
 group by event_type;
 
---Comment: Why is it "order by case eventtype" and not order by event_type case
---Comment: Within the order by case, how does sql know to show the names not the numbers
 
 --Q4. Calculate Average Revenue Per Active User per day
 -- ARPU = daily_revenue / daily_active_users
 
-select e.event_date as date, COALESCE(sum(p.revenue),0) as revenue, count(distinct(e.user_id)) as dau,
-COALESCE(sum(p.revenue),0)/count(distinct(e.user_id)) as arpu
-from events e
-left join purchases p on e.event_date=p.purchase_date
-group by e.event_date
-order by date;
+--Aggregate first due to different grains
+with dau as
+	(select event_date, count(distinct(user_id)) as dau
+	from events
+	group by event_date),
 
--- Check
+revenue as
+	(select purchase_date, sum(revenue) revenue
+	from purchases
+	group by purchase_date)
+
+select 
+	d.event_date as date,
+	coalesce(r.revenue,0),
+	d.dau,
+	coalesce(r.revenue,0)::float/d.dau as arpu
+	from dau d
+	left join revenue r on d.event_date=r.purchase_date;
 
 --Q5. Calculate 7-day rolling active users.
-select date, sum(active_users) over(order by date rows 6 preceding) as rolling_active_users
-from
-(select event_date as date, count(distinct(user_id)) as active_users
-	from events
-	group by event_date) t;
+
+--self-join method
+select e1.event_date as date, count(distinct(e2.user_id)) as rolling_active_users
+	from events e1
+	join events e2 on e2.event_date>=e1.event_date - interval '6 days' and e2.event_date<=e1.event_date
+	group by e1.event_date
+	order by date
 	
 --Q6. Find users in the top 10% of activity based on number of events.
-
 With activity_rank as
 
 (select user_id, count(event_type) as event_count,
@@ -176,24 +207,25 @@ from events
 group by user_id)
 
 select * from activity_rank
-where percentile_rank<='0.1';
+where percentile_rank>=0.9;
 
 --Q7. Using the experiments table, calculate purchase conversion rate for control and treatment
 select ex.experiment_group, 
 	count(distinct(ex.user_id)) as users, 
-	count(distinct(p.user_id)) as purchasers, 
-	count(distinct(p.user_id))/count(distinct(ex.user_id))::float as conversion_reate
+	count(distinct case when p.purchase_date>=ex.experiment_start then p.user_id end) as purchasers, 
+	count(distinct case when p.purchase_date>=ex.experiment_start then p.user_id end)/count(distinct(ex.user_id))::float as conversion_reate
 	from experiments ex
 	left join purchases p on ex.user_id=p.user_id
 	group by ex.experiment_group;
 
---Comment: A left join is used because we want to include all users in both experiment groups regardless of purchases
+--Comment--
+--A left join is used because we want to include all users in both experiment groups regardless of purchases
 
 --Q8. Calculate the average revenue per user for Calculate the average revenue per user for control vs treatment. Then compute Lift. (lift = (treatment - control) / control)
 
 -- Part 1: Calculation of average revenues
 select ex.experiment_group as group,
-	sum(p.revenue)/count(u.user_id) as arpu
+	sum(p.revenue)/count(distinct(u.user_id)) as arpu
 	from experiments ex
 	join users u on u.user_id=ex.user_id
 	left join purchases p on u.user_id=p.user_id
@@ -202,30 +234,28 @@ select ex.experiment_group as group,
 -- Part 2: lift Calculation of average revenues
 with tc_arpu as
 (select ex.experiment_group as group,
-	sum(p.revenue)/count(u.user_id) as arpu
+	sum(p.revenue)/count(distinct(u.user_id)) as arpu
 	from experiments ex
 	join users u on u.user_id=ex.user_id
 	left join purchases p on u.user_id=p.user_id
 	group by ex.experiment_group)
 
-
 select 
     (treatment_arpu - control_arpu) / control_arpu as lift
 FROM (
     select
-        avg(CASE WHEN "group" = 'treatment' THEN arpu END) AS treatment_arpu,
-        avg(CASE WHEN "group" = 'control' THEN arpu END) AS control_arpu
+        Max(CASE WHEN "group" = 'treatment' THEN arpu END) AS treatment_arpu,
+        Max(CASE WHEN "group" = 'control' THEN arpu END) AS control_arpu
     FROM tc_arpu
 );
 
--- Comments
+-- Comments--
 -- Nested Queries using CASE
 -- Isolate: Use CASE statements to pull the values into their own "buckets" (columns).
--- Collapse: Use an aggregate like MAX or SUM to squish those rows together so both values sit on the same line.
--- Calculate: Now that they are "side-by-side" in the same row, you can run your lift formula.
+-- Collapse: Use an aggregate like MAX to squish those rows together so both values sit on the same line.
+-- Calculate: Now that they are "side-by-side" in the same row, the lift formula can be executed.
 
 --Q9. Calculate the average number of days between events for each user
-
 select user_id, avg(days_between_events)::float as avg_days_between_events
 from
 	(select user_id, event_date-lag(event_date) over(partition by user_id order by event_date) as days_between_events
@@ -234,20 +264,10 @@ group by user_id;
 
 --Q10. Define churn as: no activity for 30 days
 --Find users whose last event was more than 30 days ago.
-
---Method 1
-select * from
-	(Select user_id, max(event_date) as last_activity_date
-	from events
-	group by user_id)
-where last_activity_date < now() - interval '30 days'
-order by user_id
-
---Method 2
 select * from
 	(Select u.user_id, max(e.event_date) as last_activity_date
 	from users u
 	left join events e on u.user_id=e.user_id
 	group by u.user_id)
-where last_activity_date < now() - interval '30 days'
+where last_activity_date is null or last_activity_date < now() - interval '30 days'
 order by user_id
